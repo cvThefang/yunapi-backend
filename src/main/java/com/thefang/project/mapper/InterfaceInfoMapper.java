fa5ea6bb4a0d840a21648a4c5ad070b22d39c866
Thefang <178805417@qq.com>
2025-07-15 22:51:20 +0800

    feat(backend): 新增接口信息
    1. 接口信息的增删改查
    2. 数据库建表以及 插入mock数据

---
+++ src/main/java/com/thefang/project/mapper/InterfaceInfoMapper.java
@@ -0,0 +1,18 @@
+package com.thefang.project.mapper;
+
+import com.thefang.project.model.entity.InterfaceInfo;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author Thefang
+* @description 针对表【interface_info(接口信息)】的数据库操作Mapper
+* @createDate 2025-07-15 15:34:11
+* @Entity com.thefang.project.model.entity.InterfaceInfo
+*/
+public interface InterfaceInfoMapper extends BaseMapper<InterfaceInfo> {
+
+}
+
+
+
+
---
+++ src/main/java/com/thefang/project/service/InterfaceInfoService.java
@@ -0,0 +1,20 @@
+package com.thefang.project.service;
+
+import com.thefang.project.model.entity.InterfaceInfo;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * @author Thefang
+ * @description 针对表【interface_info(接口信息)】的数据库操作Service
+ * @createDate 2025-07-15 15:34:11
+ */
+public interface InterfaceInfoService extends IService<InterfaceInfo> {
+
+    /**
+     * 验证接口信息是否有效
+     *
+     * @param interfaceInfo 接口信息
+     * @param b 是否为创建时校验
+     */
+    void validInterfaceInfo(InterfaceInfo interfaceInfo, boolean b);
+}
